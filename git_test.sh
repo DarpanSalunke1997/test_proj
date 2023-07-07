2ac47f46c47894b332e73d0f95e940f14920a873
mihirCP <mihir.cn@cerebital.com>
2023-07-07 13:15:02 +0530

    Add git_test.sh file

---
+++ git_test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Start"
+
+# pid=$(ps aux | grep '[d]jango' | awk '{print $2}')
+# echo $(ps aux)
+# kill -9 $(lsof -t -i:8000)
+cd ..
+git pull origin development
+cd crbrm/
+python3 manage.py migrate
+python3 manage.py makemigrations
+# python3 manage.py runserver
+
+echo "Done"
