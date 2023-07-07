3b8874c82e17259ec0fc7da02afeb9a1f2d5b7f9
mihirCP <mihir.cn@cerebital.com>
2023-07-07 13:35:16 +0530

    Added model

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class NewModel(models.Model):
+    name = models.CharField(default="", max_length=150, null=True, blank=True)
+    email = models.EmailField(default="", max_length=150, null=True, blank=True)
+    pnumber = models.CharField(default="", max_length=10, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
